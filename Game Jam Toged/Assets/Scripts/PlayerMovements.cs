8fb7b68b161b99444b62933febf1274d6bbe82c1
Betul KAYA <55402564+bbetulkaya@users.noreply.github.com>
2021-11-05 23:44:09 +0300

    + Refactore : refactore player jump and horizontal movement

--- Game Jam Toged/Assets/Scripts/PlayerMovements.cs
+++ Game Jam Toged/Assets/Scripts/PlayerMovements.cs
@@ -6,10 +6,14 @@ public class PlayerMovements : MonoBehaviour
 {
     [SerializeField] public float speed = 5f;
     [SerializeField] public float sideSpeed = 2f;
+    [SerializeField] public float jumpSpeed = 10f;
     [SerializeField] public float horizantalMultiplier = 2f;
 
     private Rigidbody rb;
     private float _horizontalInput;
+    private bool _jumpInput;
+    private bool _isInBoundary;
+    private bool _isPlayerCanMove;
 
     void Start()
     {
@@ -21,31 +25,32 @@ public class PlayerMovements : MonoBehaviour
         Vector3 forwardMove = transform.forward * speed * Time.fixedDeltaTime;
         Vector3 horizontalMove = transform.right * sideSpeed * _horizontalInput * Time.fixedDeltaTime;
 
+        if (_jumpInput)
+        {
+            rb.velocity = transform.up * jumpSpeed;
+            //Apply a force to this Rigidbody in direction of this GameObjects up axis
+            // rb.AddForce(transform.up * jumpSpeed, ForceMode.Impulse);
+        }
+
         // if the player is in boundary move normal
-        if (this.gameObject.transform.position.x >= LevelBoundary.leftSide &&
-            this.gameObject.transform.position.x <= LevelBoundary.rightSide)
+        if (_isInBoundary)
         {
             rb.MovePosition(rb.position + forwardMove + horizontalMove);
         }
-        // if player not in boundary check three different state
+
+        // Check player is allowed to move again if it is then move
+        else if (_isPlayerCanMove)
+        {
+
+            rb.MovePosition(rb.position + forwardMove + horizontalMove);
+            
+
+        }
+
+        // if neither one of them only allow to only forward movement
         else
         {
-            // check if player in left boundary and press right direction then allow to player move
-            if (this.gameObject.transform.position.x <= LevelBoundary.leftSide && _horizontalInput > 0)
-            {
-                rb.MovePosition(rb.position + forwardMove + horizontalMove);
-
-            }
-            // check if player in right boundary and press left direction then allow to player move
-            else if (this.gameObject.transform.position.x >= LevelBoundary.rightSide && _horizontalInput < 0)
-            {
-                rb.MovePosition(rb.position + forwardMove + horizontalMove);
-            }
-            // if neither one of them only allow to forward movement
-            else
-            {
-                rb.MovePosition(rb.position + forwardMove);
-            }
+            rb.MovePosition(rb.position + forwardMove);
         }
     }
 
@@ -53,5 +58,41 @@ public class PlayerMovements : MonoBehaviour
     void Update()
     {
         _horizontalInput = Input.GetAxis("Horizontal");
+        _jumpInput = Input.GetKeyDown(KeyCode.Space);
+
+        _isInBoundary = IsPlayerInBoundary();
+        _isPlayerCanMove = IsPlayerAllowedToMove();
+    }
+
+    bool IsPlayerInBoundary()
+    {
+        if (this.gameObject.transform.position.x >= LevelBoundary.leftSide &&
+        this.gameObject.transform.position.x <= LevelBoundary.rightSide)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    bool IsPlayerAllowedToMove()
+    {
+        if (this.gameObject.transform.position.x <= LevelBoundary.leftSide && _horizontalInput > 0)
+        {
+            return true;
+
+        }
+        // check if player in right boundary and press left direction then allow to player move
+        else if (this.gameObject.transform.position.x >= LevelBoundary.rightSide && _horizontalInput < 0)
+        {
+            return true;
+
+        }
+        else
+        {
+            return false;
+        }
     }
 }
